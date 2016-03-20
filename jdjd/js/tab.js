c17c5818524eeeda5ea93db13eed11a002724740
zwq1987 <qiang511130957@qq.com>
2016-03-20 14:22:22 +0800

    移动端

--- jdjd/js/tab.js
+++
@@ -1,5 +0,0 @@
-$(".tab-tip li").on("mouseover",function(){
-    var $index=$(this).index();
-    $(this).addClass("select").siblings().removeClass("select");
-    $(this).parents(".mt").next(".mc").children(".tab_r").children().removeClass("show-tab_r").eq($index).addClass("show-tab_r");
-});
